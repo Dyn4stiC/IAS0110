fb20208de9559f800f7762cd5763c8b5ba8eeb48
Dyn4stiC <oliver.sild2002@gmail.com>
2024-01-31 13:29:43 +0200

    Converter working

--- PrjConverterForm/FormConverter.vb
+++ PrjConverterForm/FormConverter.vb
@@ -1,9 +1,13 @@
 ﻿Public Class FormConverter
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    End Sub
-
-    Private Sub FormConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Const KM2MI_CONV_RATE = 1.609344
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Try
+            txtMi.Text = txtKm.Text / KM2MI_CONV_RATE
+        Catch ex As Exception
+            'MsgBox("Tekkis viga!", MsgBoxStyle.Critical, "KÕIK ON KATKI!!!")
+            txtMi.Text = "Sisendi viga!"
+        End Try
 
     End Sub
 End Class
