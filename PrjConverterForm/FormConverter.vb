3c468696d0c19c992fafcff692005e810cc896f2
Dyn4stiC <oliver.sild2002@gmail.com>
2024-01-31 13:43:16 +0200

    Kellaaeg

--- PrjConverterForm/FormConverter.vb
+++ PrjConverterForm/FormConverter.vb
@@ -10,4 +10,29 @@
         End Try
 
     End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub FormConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Timer südmus "tick" käivitatakse iga sek tagant
+        timerClock.Interval = 1000
+
+        ' Lülitame timeri sisse
+        timerClock.Enabled = True
+    End Sub
+
+    Private Sub VäljuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VäljuToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub timerClock_Tick(sender As Object, e As EventArgs) Handles timerClock.Tick
+        lblClock.Text = DateTime.Now
+    End Sub
+
+    Private Sub lblClock_Click(sender As Object, e As EventArgs) Handles lblClock.Click
+        timerClock.Enabled = Not timerClock.Enabled
+        lblClock.Text = "meow"
+    End Sub
 End Class
