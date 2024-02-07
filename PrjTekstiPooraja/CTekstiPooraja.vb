950bf86f533934befcbf468c61dd9d9525396d6a
dyn4stic <oliver.sild2002@gmail.com>
2024-02-07 13:15:07 +0200

    Class Meetodid

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -1,3 +1,50 @@
 ﻿Public Class CTekstiPooraja
+    Implements ITeisendused
 
+    'Klassi CTesktiPooraja atribuudid (muutujad)
+    Private intAlgusSymbol As Integer
+    Private intLoppSymbol As Integer
+    Private strPooratavTekst As String
+
+
+    'Liidese omaduste seadistamine
+    'Atribuut intAlgus seadistamine
+    Private Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+            Return intAlgusSymbol 'küsimisel tagasta intAlgusSymbol
+        End Get
+        Set(ByVal value As Integer)
+            intAlgusSymbol = value 'atribuudi väärtuse seadistamine
+        End Set
+    End Property
+
+    'Atribuut intLopp seadistamine
+    Private Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+            Return intLoppSymbol
+        End Get
+        Set(ByVal value As Integer)
+            intLoppSymbol = value
+        End Set
+    End Property
+
+    'Atribuut strTekst seadistamine
+    Private Property strTekst As Integer Implements ITeisendused.strTekst
+        Get
+            Return strPooratavTekst
+        End Get
+        Set(value As Integer)
+            strPooratavTekst = value
+        End Set
+    End Property
+
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) _
+        Implements ITeisendused.teisendaTekst
+        strPooratavTekst = StrReverse(strSisendTekst)
+    End Sub
+
+    Private Function pooraTekst() As String _
+        Implements ITeisendused.pooraTekst
+        Return StrReverse(strPooratavTekst)
+    End Function
 End Class
--- PrjTekstiPooraja/ITeisendused.vb
+++ PrjTekstiPooraja/ITeisendused.vb
@@ -1,3 +1,10 @@
 ﻿Public Interface ITeisendused
+    Property intAlgus As Integer 'teksti esimene sümbol ASCII koodi jaoks
+    Property intLopp As Integer 'teksti viimane sümbol ASCII koodi jaoks
+    Property strTekst As Integer 'Kasutaja sisestatud teksti jaoks
+
+    Function pooraTekst() As String 'Liidese meetodid koodi ei oma
+    Sub teisendaTekst(ByRef strSisendTekst As String)
+
 
 End Interface
