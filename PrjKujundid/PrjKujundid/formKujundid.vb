38a2c862e673a0286fe874d85a65915450d1c490
ol.sild <oliver.sild2002@gmail.com>
2024-02-28 13:59:04 +0200

    Nuppude syndmused

--- PrjKujundid/PrjKujundid/formKujundid.vb
+++ PrjKujundid/PrjKujundid/formKujundid.vb
@@ -6,4 +6,24 @@
         txtPindala.Text = kujund.leiaPindala
         txtYmbermoot.Text = kujund.leiaYmberMoot
     End Sub
+
+    Private Sub btnKolmnurk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles btnKolmnurk.Click
+
+        Dim objektKolmnurk As New Kolmnurk(txtKylgA.Text, txtKylgB.Text)
+        tootleKujund(objektKolmnurk)
+    End Sub
+
+    Private Sub btnRuut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles btnRuut.Click
+
+        Dim objektRuut As New Ruut(txtKylgA.Text)
+        tootleKujund(objektRuut)
+    End Sub
+
+    Private Sub btnRistkylik_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles btnRistkylik.Click
+
+        tootleKujund(New Ristkylik(txtKylgA.Text, txtKylgB.Text))
+    End Sub
 End Class
