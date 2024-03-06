99706de288a24b45562831cf1e826d4aa3159fe5
olsild <oliver.sild2002@gmail.com>
2024-03-06 12:20:53 +0200

    Uue projekti lisamine

---
+++ PrjTekstiOmadusedTestid/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("c0147161-8a27-4606-884a-17a9ace2a813")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
